12c54f5d054da29692773d7f21889c166d1b4cd5
babalolabrainiac <babalolaopedaniel@gmail.com>
2021-09-17 17:42:32 +0100

    Updated APP

---
+++ routes/transaction.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const paystack = require("../controllers/paystack");
+const Transaction = require("../models/transaction");
+const User = require("../models/user");
+const Send = require("../models/account");
--- routes/user.js
+++ routes/user.js
@@ -4,11 +4,12 @@ const User = require("../models/user");
 const mongoose = require("mongoose");
 const config = require("../config/config");
 const errors = require("restify-errors");
-const mysql = require("mysql");
+const mysql = require("mysql2");
+const bcrypt = require("bcrypt");
 const { nanoid } = require("nanoid");
 
 //Setup MySql
-const db = mysql.createConnection({
+const pool = mysql.createPool({
   host: "localhost",
   user: "root",
   port: "3306",
@@ -16,34 +17,63 @@ const db = mysql.createConnection({
   database: "simple_fintech",
 });
 
-//Connect to MySQL Database
-db.connect((err) => {
-  if (err) {
-    return new errors.InternalServerError("Could Not Connect To Database");
-  }
-  console.log("Server Connected Successfully");
-});
-
 //Signup New User
 router.post("/signup", (req, res, next) => {
-  const user = new User({
-    user_id: new mongoose.Types.ObjectId(),
-    name: req.body.name,
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-    account_id: nanoid(10),
-    withdrawal_bank: "Not set",
-    w_bank_account: "",
-    beneficiaries: "None",
-  });
-  let sql = "INSERT INTO users set ?";
-  let query = db.query(sql, user, (err, result) => {
+  bcrypt.hash(req.body.password, 10, (err, hash) => {
     if (err) {
-      console.log(err);
+      return new errors.InternalServerError("Password Error");
+    } else {
+      const user = new User({
+        user_id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        username: req.body.username,
+        email: req.body.email,
+        password: hash,
+        account_id: nanoid(),
+        withdrawal_bank: "Not set",
+      });
+      let sql = `INSERT INTO users VALUES('${user.user_id}', '${user.name}', '${user.password}', '${user.name}', '${user.email}', '${user.accountn_id}', '${user.account_balance}', '${user.withdrawal_bank}', '${user.w_bank_account}', '${user.beneficiaries}')`;
+      pool.execute(sql, (err, result) => {
+        if ((err, result)) {
+          if (err) {
+            res.status(201).json({
+              Error: err,
+              Error_Msg: "Signup Not Successful",
+            });
+          }
+        }
+        console.log(result);
+        res.status(200).json("User Saved to DB Successfully");
+      });
     }
-    console.log(result);
-    res.status(200).json("User Saved to DB Successfully");
+  });
+});
+
+//Fetch Users From Database
+router.get("/getusers", (req, res, next) => {
+  User.find();
+  let sql = "SELECT * from users";
+  pool.execute(sql, (err, users) => {
+    if (err) {
+      res.status(500).json("Unable to retrieve Users");
+    } else {
+      console.log(users);
+      res.status(200).json("Users Successfully Retrieved");
+    }
+  });
+});
+
+//Fetch Specific User
+router.post("/:user_id", (req, res, next) => {
+  let id = req.params.user_id;
+  const sql = `SELECT * FROM users WHERE user_id = ${id}`;
+  pool.execute(sql, (err, user) => {
+    if (err) {
+      return new errors.ResourceNotFoundError("User Not Found");
+    } else {
+      console.log(user);
+      res.status(200).json("User Successfully Retrieved");
+    }
   });
 });
 
