1c8228c56c20cbea893f7cfdf31cfd3fb05c24b1
babalolabrainiac <babalolaopedaniel@gmail.com>
2021-09-23 08:28:33 +0100

    Exports DB Module

--- controllers/db.js
+++ controllers/db.js
@@ -1,12 +1,13 @@
 const mysql = require("mysql2");
+const config = require('../config/config')
 
 //Setup MySql
 const pool = mysql.createPool({
   host: "localhost",
   user: "root",
   port: "3306",
-  password: process.env.MySQL_Password,
+  password: config.MySQL_Password,
   database: "simple_fintech",
 });
 
-module.exports = pool.Promise();
+module.exports = pool;
